edc03632a893ceda92d8ce3d8b56e9a5638d6750
Mauriciojjoaquim <mauriciojjoaquim@msn.com>
2022-11-02 18:34:44 -0300

    quarta parte

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from utils.recipe.factory import make_recipe
 
 # Create your views here.
 
@@ -8,5 +9,12 @@ from django.shortcuts import render
 # http request
 def home(request):
    return render(request, 'recipes/pages/home.html', context={
-    'name': 'Mauricio J Joaquim',
+    'recipes': [make_recipe() for _ in range(10)],
+   })
+
+   # http request
+def recipe(request, id):
+   return render(request, 'recipes/pages/recipe-view.html', context={
+    'recipe': make_recipe(),
+    'is_detail_page': True,
    })
