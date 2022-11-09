ab93c519773a546f51c9f897b8436d6a07e72482
Mauriciojjoaquim <mauriciojjoaquim@msn.com>
2022-11-08 22:41:08 -0300

    nono

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,6 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, get_list_or_404,get_object_or_404
+from recipes.models import Recipe
 from utils.recipe.factory import make_recipe
 
 # Create your views here.
@@ -10,8 +9,23 @@ from utils.recipe.factory import make_recipe
 
 # http request
 def home(request):
+   recipes = Recipe.objects.filter(
+      is_published=True,
+   ).order_by('-id')
    return render(request, 'recipes/pages/home.html', context={
-    'recipes': [make_recipe() for _ in range(10)],
+    'recipes': recipes,
+   })
+
+   # http request
+def category(request, category_id):
+   recipes = get_list_or_404(Recipe.objects.filter(
+      category__id=category_id,
+      is_published=True,
+   ).order_by('-id'))     
+
+   return render(request, 'recipes/pages/category.html', context={
+    'recipes': recipes,
+    'title': f'{recipes[0].category.name} - CATEGORY | ',
    })
 
    # http request
