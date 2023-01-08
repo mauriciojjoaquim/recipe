330a710b9b24e307ff198f5072daeb7772d70e34
Mauriciojjoaquim <mauriciojjoaquim@msn.com>
2023-01-08 20:05:08 -0300

    V10.2010 inicio test

--- authors/forms.py
+++ authors/forms.py
@@ -1,21 +1,36 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms import widgets
+from django.core.exceptions import ValidationError
+import re
 
 def add_attr(field, attr_name, attr_new_val):
     existing_attr = field.widget.attrs.get(attr_name, '')
     field.widget.attrs[attr_name] = f'{existing_attr}{attr_new_val}'.strip()
     
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val    
+    add_attr(field, 'placeholder', placeholder_val)
+    
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+    
+    if not regex.match(password):
+        raise ValidationError((
+            'password must have at least one oppercase, '
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='Invalid'
+        )
 
 class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        add_placeholder(self.fields['username'], 'Your username')
-        add_placeholder(self.fields['email'], 'Your e-mail')
-        add_placeholder(self.fields['first_name'], 'Ex.: John')
-        add_placeholder(self.fields['last_name'], 'Ex.: Doe')
+        add_placeholder(self.fields['username'], ' Your username')
+        add_placeholder(self.fields['email'], ' Your e-mail')
+        add_placeholder(self.fields['first_name'], ' Ex.: John')
+        add_placeholder(self.fields['last_name'], ' Ex.: Doe')
+        add_attr(self.fields['username'], 'css', 'a-css-class')
         
     password = forms.CharField(
         required=True,
@@ -29,7 +44,8 @@ class RegisterForm(forms.ModelForm):
             'password must have at least one oppercase, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
@@ -74,3 +90,46 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Type your password here.'
             }),
         }
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+        
+        if 'atenção' in data:
+            raise ValidationError(
+                'Não Digite %(value)s no campo password',
+                code='invalid',
+                params={ 'value': '"atenção"'}
+            )
+        
+        return data
+    
+    def clean_first_name(self):
+        data = self.cleaned_data.get('first_name')
+        
+        if 'John Doe' in data:
+            raise ValidationError(
+                'Não Digite %(value)s no campo first name',
+                code='invalid',
+                params={ 'value': '"John Doe"'}
+            )
+        
+        return data
+    
+    def clean(self):
+        cleaned_data = super().clean()
+        
+        password = cleaned_data.get('password')
+        password2 = cleaned_data.get('password2')
+        
+        if password != password2:
+            password_confimation_error = ValidationError(
+                'password and password2 must be equal',
+                code='invalid'
+            )
+            raise ValidationError({
+                    'password': password_confimation_error,
+                    'password2': [
+                        password_confimation_error,
+                     ],
+                })
+            
+        
